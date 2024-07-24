96d6564d36ebb3ff8cfc46effd588ad457ff20b9
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-07-24 23:45:02 +0530

    ut [doctors row/category row]

--- Frondend/src/Components/User/CategoriesRow/CategoriesRow.jsx
+++ Frondend/src/Components/User/CategoriesRow/CategoriesRow.jsx
@@ -1,11 +1,79 @@
-import React from 'react'
+import React from "react";
+import "./CategoriesRow.css";
 
 const CategoriesRow = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="category-row">
+      <h2>Categories</h2>
+      <div className="category-row-container">
+        <div className="categoryText-container">
+          <h2 className="cat-title">categoriiiiiiiiii</h2>
+          <p className="cat-discription">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor,
+            libero nec lacinia
+          </p>
+        </div>
+      </div>
 
-export default CategoriesRow
+      <div className="category-row-container">
+        <div className="categoryText-container">
+          <h2 className="cat-title">categoriiiiiiiiii</h2>
+          <p className="cat-discription">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor,
+            libero nec lacinia
+          </p>
+        </div>
+      </div>
+
+      <div className="category-row-container">
+        <div className="categoryText-container">
+          <h2 className="cat-title">categoriiiiiiiiii</h2>
+          <p className="cat-discription">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor,
+            libero nec lacinia
+          </p>
+        </div>
+      </div>
+
+      <div className="category-row-container">
+        <div className="categoryText-container">
+          <h2 className="cat-title">categoriiiiiiiiii</h2>
+          <p className="cat-discription">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor,
+            libero nec lacinia
+          </p>
+        </div>
+      </div>
+
+      <div className="category-row-container">
+        <div className="categoryText-container">
+          <h2 className="cat-title">categoriiiiiiiiii</h2>
+          <p className="cat-discription">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor,
+            libero nec lacinia
+          </p>
+        </div>
+      </div>
+      <div className="category-row-container">
+        <div className="categoryText-container">
+          <h2 className="cat-title">categoriiiiiiiiii</h2>
+          <p className="cat-discription">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor,
+            libero nec lacinia
+          </p>
+        </div>
+      </div>
+      <div className="category-row-container">
+        <div className="categoryText-container">
+          <h2 className="cat-title">categoriiiiiiiiii</h2>
+          <p className="cat-discription">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed auctor,
+            libero nec lacinia
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoriesRow;
