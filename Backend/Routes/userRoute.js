e7c77c0dbf32e0a9354bbdb270cd7d0d48727764
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-07-27 16:58:14 +0530

    minor changes

--- Backend/Routes/userRoute.js
+++ Backend/Routes/userRoute.js
@@ -2,11 +2,12 @@ const express = require("express");
 const userRouter = express.Router();
 const userController = require("../Controllers/userController");
 
-const { userRegister, verifyOtp, verifyResendOtp } = userController;
+const { userRegister, verifyOtp, verifyResendOtp, userLogin } = userController;
 
 userRouter
   .post("/register", userRegister)
   .post("/otp", verifyOtp)
-  .post("/resendOtp", verifyResendOtp);
+  .post("/resendOtp", verifyResendOtp)
+  .post("/login", userLogin);
 
 module.exports = userRouter;
