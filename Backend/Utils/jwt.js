ba2e9857a586fb09260f961a0a7d0b9c32b7ffe5
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-08-02 23:42:13 +0530

    jwt verification

--- Backend/Utils/jwt.js
+++ Backend/Utils/jwt.js
@@ -9,4 +9,26 @@ const createToken = (userId) => {
   return token;
 };
 
-module.exports = { createToken };
+const verifyToken = (req, res, next) => {
+  try {
+    const authHeader = req.headers["authorization"];
+    if (!authHeader) {
+      return res.status(401).json({ message: "No token provided" });
+    }
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+    jwt.verify(token, process.env.Jwt_Secret_Key, (err, decoded) => {
+      if (err) {
+        return res.status(403).json({ message: "Invalid token" });
+      } else {
+        next();
+      }
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = { createToken ,verifyToken };
