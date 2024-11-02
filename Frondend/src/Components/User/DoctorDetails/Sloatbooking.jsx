3170bc08f4d6ce55c53b4c5849b72ccc473541bb
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-11-02 20:32:30 +0530

    minor changes

--- Frondend/src/Components/User/DoctorDetails/Sloatbooking.jsx
+++ Frondend/src/Components/User/DoctorDetails/Sloatbooking.jsx
@@ -19,17 +19,17 @@ const Sloatbooking = ({ closeModal, doctorid }) => {
 
 
 
-  // const slotStatuses = useMemo(
-  //   () => [
-  //     { color: "bg-green-600 border-green-400", label: "selected" },
-  //     { color: "bg-[#eaff45] border-yellow-300", label: "available" },
-  //     {
-  //       color: "bg-[#989898] border-l-neutral-400",
-  //       label: "not available",
-  //     },
-  //   ],
-  //   []
-  // );
+  const slotStatuses = useMemo(
+    () => [
+      { color: "bg-green-600 border-green-400", label: "selected" },
+      { color: "bg-[#eaff45] border-yellow-300", label: "available" },
+      {
+        color: "bg-[#989898] border-l-neutral-400",
+        label: "not available",
+      },
+    ],
+    []
+  );
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-60 flex justify-center items-center z-50">
@@ -41,14 +41,14 @@ const Sloatbooking = ({ closeModal, doctorid }) => {
           X
         </button>
         <h2 className="text-md font-bold">Select a sloat</h2>
-        {/* <div className="flex gap-5">
+        <div className="flex gap-5">
           {slotStatuses.map((data, index) => (
             <div className="flex gap-1 justify-center items-center" key={index}>
               <div className={`w-3 h-3 border ${data.color}`}></div>
               <span className="text-sm">{data.label}</span>
             </div>
           ))}
-        </div> */}
+        </div> 
         <div className="items-center justify-center  flex m-3">
           <Calender
             className="border-none rounded-lg bg-white shadow-lg p-2"
