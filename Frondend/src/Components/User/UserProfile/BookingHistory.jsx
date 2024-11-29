f60e26c5bc19eaf33d654264fae7686a5c618bee
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-11-29 21:13:18 +0530

    fetch history booking

--- Frondend/src/Components/User/UserProfile/BookingHistory.jsx
+++ Frondend/src/Components/User/UserProfile/BookingHistory.jsx
@@ -1,14 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Footer, Header, HistoryViewMore, Usersidebar } from "../..";
 import dummy from "../../../assets/Public/dummy.jpg";
+import { useSelector } from "react-redux";
+import user_Api from "../../../service/Userinstance";
+import { toast } from "sonner";
 
 const BookingHistory = () => {
   const [viewMore, setViewMore] = useState(false);
+  const { userData } = useSelector((state) => state.user);
+  const [historyResult, setHistoryResult] = useState(null);
+
+  useEffect(() => {
+    const fetchHistory = async () => {
+      try {
+        const { data, status } = await user_Api.get(
+          `/view-booking-history?userId=${userData.id}`
+        );
+        if ([200].includes(status)) setHistoryResult(data);
+      } catch (error) {
+        if (error.response) {
+          const { data, status } = error.response;
+          if ([500].includes(status)) toast.error(data.message);
+        }
+      }
+    };
+    fetchHistory();
+  }, []);
+
+  const handleView = (history) => {
+    setHistoryResult(history)
+    setViewMore(true)
+  }
 
   return (
     <>
-      {!viewMore ? <Header /> : ""}
-
+      <Header />
       <div className="flex justify-center items-center p-24 gap-10">
         <Usersidebar />
         <div className="bg-white shadow-lg p-5 rounded-lg">
@@ -29,37 +55,61 @@ const BookingHistory = () => {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td className="px-4 py-2">
-                  <img src={dummy} alt="dr" className="w-9 h-9 rounded-full" />
-                </td>
-                <td className=" text-sm text-gray-700">Dr. Jasir</td>
-                <td className=" text-sm text-gray-700">12/3/2024</td>
-                {/* <td className=" text-sm ">
-                  <div className="bg-yellow-100 rounded-2xl text-center text-yellow-800 border border-yellow-600 px-1 py-1">
-                    Processing
-                  </div>
-                </td> */}
-                <td className=" text-sm ">
-                  <div className="bg-green-300 rounded-2xl text-center text-green-800 border border-green-800 px-1 py-1">
-                    Completed
-                  </div>
-                </td>
-                <td className=" text-sm text-gray-700">
-                  <button
-                    className="bg-black text-white p-1 rounded-2xl w-full"
-                    onClick={() => setViewMore(true)}
-                  >
-                    View
-                  </button>
-                </td>
-              </tr>
+              {historyResult?.map((history, index) => (
+                <tr key={index}>
+                  <td className="px-4 py-2">
+                    <img
+                      src={`../src/assets/images/${history?.doctorDetails[0]?.profileImageOfDoctor}`}
+                      alt="dr"
+                      className="w-9 h-9 rounded-full"
+                    />
+                  </td>
+                  <td className=" text-sm text-gray-700">{`Dr.${history?.doctorDetails[0]?.nameOfDoctor} ${history?.doctorDetails[0]?.degreeOfDoctor}`}</td>
+                  <td className=" text-sm text-gray-700">
+                    {history?.slots?.bookedDate}
+                  </td>
+                  <td className=" text-sm ">
+                    {history?.doctorDetails[0]?.sotBookingForPatients[0]
+                      ?.bookingStatus === "Processing" ? (
+                      <div className="bg-yellow-100 rounded-2xl text-center text-yellow-800 border border-yellow-600 px-1 py-1">
+                        {
+                          history?.doctorDetails[0]?.sotBookingForPatients[0]
+                            ?.bookingStatus
+                        }
+                      </div>
+                    ) : (
+                      <td className=" text-sm ">
+                        <div className="bg-green-300 rounded-2xl text-center text-green-800 border border-green-800 px-1 py-1">
+                          {
+                            history?.doctorDetails[0]?.sotBookingForPatients[0]
+                              ?.bookingStatus
+                          }
+                        </div>
+                      </td>
+                    )}
+                  </td>
+
+                  <td className=" text-sm text-gray-700">
+                    <button
+                      className="bg-black text-white p-1 rounded-2xl w-full"
+                      onClick={() => handleView(history)}
+                    >
+                      View
+                    </button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
       </div>
       <Footer />
-      {viewMore && <HistoryViewMore closeModal={()=> setViewMore(false)} />}
+      {viewMore && (
+        <HistoryViewMore
+          closeModal={() => setViewMore(false)}
+          viewMoreResult={historyResult}
+        />
+      )}
     </>
   );
 };
--- Frondend/src/Components/User/UserProfile/ViewMore.jsx
+++ Frondend/src/Components/User/UserProfile/ViewMore.jsx
@@ -1,37 +1,51 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-const ViewMore = ({closeModal}) => {
+const ViewMore = ({ closeModal, viewMoreResult }) => {
+  console.log("here is the datea", viewMoreResult);
+  useEffect(() => {
+    AOS.init({ duration: 700 });
+  }, []);
+
+  const handleCancelBooking = ({}) => {};
+
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-80 flex justify-center items-center">
-      <div className="relative bg-white shadow-lg p-6 rounded-xl flex flex-col">
-        <div className="absolute top-[-0.75rem] right-[-9px] bg-slate-900 text-white rounded-full text-center w-7 h-7 flex justify-center items-center cursor-pointer" onClick={closeModal}>
+    <div
+      className="fixed top-28 right-10 flex justify-center items-center"
+      data-aos="fade-down"
+    >
+      <div className="relative bg-white shadow-2xl p-6 rounded-xl flex flex-col border">
+        <div
+          className="absolute top-[-0.75rem] right-[-9px] bg-slate-900 text-white rounded-full text-center w-7 h-7 flex justify-center items-center cursor-pointer text-sm"
+          onClick={closeModal}
+        >
           x
         </div>
         <div className="bg-slate-300 p-2 rounded-lg border border-slate-400">
-          <span>Booking id:</span> <span>kkkkkkkiiiiiiiiiiii99999999999</span>
+          <span>Booking id:</span> <span>{viewMoreResult?.bookingId}</span>
         </div>
         <div className="bg-white mt-2 flex flex-col justify-center items-center shadow-lg p-4 rounded-lg">
           <div>
             <div>
-              <p>Booking Date : 12/3/2024</p>
+              <p>Booking Date : {viewMoreResult?.slots?.bookedDate}</p>
             </div>
             <div className="grid grid-cols-2 gap-2 mt-3">
-              <p className="bg-yellow-200 border border-yellow-600 rounded-md text-center">
-                hello
-              </p>
-              <p className="bg-yellow-200 border border-yellow-600 rounded-md text-center">
-                hello
-              </p>
-              <p className="bg-yellow-200 border border-yellow-600 rounded-md text-center">
-                hello
-              </p>
-              <p className="bg-yellow-200 border border-yellow-600 rounded-md text-center">
-                hello
-              </p>
+              {viewMoreResult?.slots?.BookedSeats?.map((slot, index) => (
+                <p
+                  className="bg-yellow-200 border border-yellow-600 rounded-md text-center"
+                  key={index}
+                >
+                  {slot}
+                </p>
+              ))}
             </div>
           </div>
           <div className="mt-3">
-            <button className="bg-black text-white w-full rounded-xl px-2 py-1">
+            <button
+              className="bg-black text-white w-full rounded-xl px-4 py-1 text-sm"
+              onClick={handleCancelBooking}
+            >
               cancel booking
             </button>
           </div>
