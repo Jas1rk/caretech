0dc9f7c792cb567dc287807735514f08ddc9f7b4
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-12-01 14:41:59 +0530

    $lookup

--- Backend/Controllers/Doctor/bookingDetails.js
+++ Backend/Controllers/Doctor/bookingDetails.js
@@ -3,19 +3,28 @@ const { ObjectId } = require("mongodb");
 
 const fetchUsersBookings = async (req, res) => {
   try {
-    const {doctor_id} = req.query;
+    const { doctor_id } = req.query;
     const fetchDetails = await Doctor.aggregate([
       { $match: { _id: new ObjectId(doctor_id) } },
       { $unwind: "$sotBookingForPatients" },
-      { $project: { sotBookingForPatients: 1 } },
+      {
+        $lookup: {
+          from: "users",
+          localField: "sotBookingForPatients.userId",
+          foreignField: "_id",
+          as: "userDetails",
+        },
+      },
+      { $project: { sotBookingForPatients: 1, userDetails: 1 } },
     ]);
     console.log("here is==", fetchDetails);
+    res.status(200).json(fetchDetails);
   } catch (error) {
     res.status(500).json(error.message);
   }
 };
 
-/// lookup userdetails
+
 
 const changeStatus = async (req, res) => {
   try {
