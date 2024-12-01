7b84e2f8b95e1acfae5115cb001bf6cb895a6b69
Jasir k <149037479+Jas1rk@users.noreply.github.com>
2024-12-01 22:36:50 +0530

    error fixed pendig change status

--- Backend/Controllers/Doctor/bookingDetails.js
+++ Backend/Controllers/Doctor/bookingDetails.js
@@ -1,30 +1,47 @@
 const Doctor = require("../../Model/doctorModel");
+const Booking = require("../../Model/BookingModel");
 const { ObjectId } = require("mongodb");
+const { default: mongoose } = require("mongoose");
 
 const fetchUsersBookings = async (req, res) => {
   try {
     const { doctor_id } = req.query;
-    const fetchDetails = await Doctor.aggregate([
-      { $match: { _id: new ObjectId(doctor_id) } },
-      { $unwind: "$sotBookingForPatients" },
-      {
-        $lookup: {
-          from: "users",
-          localField: "sotBookingForPatients.userId",
-          foreignField: "_id",
-          as: "userDetails",
+    const doctorId = new mongoose.Types.ObjectId(doctor_id);
+    const [fetchDetails, fetchBooking] = await Promise.all([
+      Doctor.aggregate([
+        { $match: { _id: doctorId } },
+        { $unwind: "$sotBookingForPatients" },
+        {
+          $lookup: {
+            from: "users",
+            localField: "sotBookingForPatients.userId",
+            foreignField: "_id",
+            as: "userDetails",
+          },
         },
-      },
-      { $project: { sotBookingForPatients: 1, userDetails: 1 } },
+        { $project: { sotBookingForPatients: 1, userDetails: 1 } },
+      ]),
+      Booking.aggregate([
+        { $unwind: "$slots" },
+        {
+          $match: { "slots.doctorId": doctorId },
+        },
+        {
+          $project: {
+            bookingId: 1,
+            "slots.bookedDate": 1,
+            "slots.BookedSeats": 1,
+          },
+        },
+        {$sort:{_id: -1}}
+      ]),
     ]);
-    console.log("here is==", fetchDetails);
-    res.status(200).json(fetchDetails);
+    console.log(fetchDetails, fetchBooking);
+    res.status(200).json({ fetchDetails, fetchBooking });
   } catch (error) {
     res.status(500).json(error.message);
   }
 };
-
-
 
 const changeStatus = async (req, res) => {
   try {
--- Frondend/src/App.jsx
+++ Frondend/src/App.jsx
@@ -13,6 +13,7 @@ import DoctorRoutes from "./Routes/DoctorRoutes";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
+
 function App() {
   const dispatch = useDispatch();
   useEffect(() => {
